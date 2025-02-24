6edda070b81ae43cf4c3f8ef87bc4a22ec42a5ba
Rayan Barghchoun <barghchounr@yahoo.com>
2025-02-24 15:50:58 -0600

    more class

--- src/review5/E02FirstLast.java
+++ src/review5/E02FirstLast.java
@@ -7,6 +7,17 @@ public class E02FirstLast {
 	//the array. Call your method to ensure that it works!
 	
 	public static void main(String[] args) {
-		
+		int[] testArray = {1, 2, 3, 4};
+		int result = sumFirstLast(testArray);
+		System.out.println(result);
 	}
+
+	public static int sumFirstLast(int[] array){
+		int first = array[0];
+		int last = array[array.length - 1];
+		int sum = first + last;
+
+		return sum;
+	}
+
 }
--- src/review5/E03AddArrays.java
+++ src/review5/E03AddArrays.java
@@ -11,4 +11,35 @@ public class E03AddArrays {
 	//[1, 6]
 	//	=
 	//[4, 10, 5, 1] 
+
+	public static void main(String[] args){
+		int[] first = {3, 4, 5, 1};
+		int[]second = {1, 6};
+		int[] result = addArrays(first, second);
+
+		for(int i = 0; i < result.length; i++){
+			System.out.println(result[i]);
+		}
+	}
+
+	public static int[] addArrays(int[] first, int[] second){
+		int max = first.length;
+
+		if(second.length > first.length){
+			max = second.length;
+		}
+
+		int[] result = new int[max];
+		for(int i = 0; i < max; i++){
+			if(i < first.length){
+				result[i] += first[i];
+			}
+
+			if(i < second.length){
+				result[i] += second[i];
+			}
+		}
+
+		return result;
+	}
 }
